6ba7f382e3e332ab5adb98cbcdd1cca7cc6aeec9
Rachet Guillaume <guillaume.rachet@nokia.com>
2018-11-13 12:55:21 +0100

    suggestion fix

--- src/components/PlaceSearcher.js
+++ src/components/PlaceSearcher.js
@@ -8,214 +8,184 @@ import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import MenuItem from '@material-ui/core/MenuItem';
 import {withStyles} from '@material-ui/core/styles';
+import fetch from "cross-fetch";
 
 const styles = theme => ({
-    root: {
-        padding: 20,
-        marginTop: 100,
-        flexGrow: 1,
-    },
-    container: {
-        position: 'relative',
-    },
-    suggestionsContainerOpen: {
-        position: 'absolute',
-        zIndex: 1,
-        marginTop: theme.spacing.unit,
-        left: 0,
-        right: 0,
-    },
-    suggestion: {
-        display: 'block',
-    },
-    suggestionsList: {
-        margin: 0,
-        padding: 0,
-        listStyleType: 'none',
-    },
-    divider: {
-        height: theme.spacing.unit * 2,
-    },
+  root: {
+    padding: 20,
+    marginTop: 100,
+    flexGrow: 1,
+  },
+  container: {
+    position: 'relative',
+  },
+  suggestionsContainerOpen: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0,
+  },
+  suggestion: {
+    display: 'block',
+  },
+  suggestionsList: {
+    margin: 0,
+    padding: 0,
+    listStyleType: 'none',
+  },
+  divider: {
+    height: theme.spacing.unit * 2,
+  },
 });
 
-const suggestions = [
-    {label: 'Afghanistan'},
-    {label: 'Aland Islands'},
-    {label: 'Albania'},
-    {label: 'Algeria'},
-    {label: 'American Samoa'},
-    {label: 'Andorra'},
-    {label: 'Angola'},
-    {label: 'Anguilla'},
-    {label: 'Antarctica'},
-    {label: 'Antigua and Barbuda'},
-    {label: 'Argentina'},
-    {label: 'Armenia'},
-    {label: 'Aruba'},
-    {label: 'Australia'},
-    {label: 'Austria'},
-    {label: 'Azerbaijan'},
-    {label: 'Bahamas'},
-    {label: 'Bahrain'},
-    {label: 'Bangladesh'},
-    {label: 'Barbados'},
-    {label: 'Belarus'},
-    {label: 'Belgium'},
-    {label: 'Belize'},
-    {label: 'Benin'},
-    {label: 'Bermuda'},
-    {label: 'Bhutan'},
-    {label: 'Bolivia, Plurinational State of'},
-    {label: 'Bonaire, Sint Eustatius and Saba'},
-    {label: 'Bosnia and Herzegovina'},
-    {label: 'Botswana'},
-    {label: 'Bouvet Island'},
-    {label: 'Brazil'},
-    {label: 'British Indian Ocean Territory'},
-    {label: 'Brunei Darussalam'},
-];
-
 function renderInputComponent(inputProps) {
-    const {
-        classes, inputRef = () => {
-        }, ref, ...other
-    } = inputProps;
+  const {
+    classes, inputRef = () => {
+    }, ref, ...other
+  } = inputProps;
 
-    return (
-        <TextField
-            autoFocus
-            variant={"filled"}
-            fullWidth
-            InputProps={{
-                inputRef: node => {
-                    ref(node);
-                    inputRef(node);
-                },
-                classes: {
-                    input: classes.input,
-                },
-            }}
-            {...other}
-        />
-    );
+  return (
+    <TextField
+      autoFocus
+      variant={"filled"}
+      fullWidth
+      InputProps={{
+        inputRef: node => {
+          ref(node);
+          inputRef(node);
+        },
+        classes: {
+          input: classes.input,
+        },
+      }}
+      {...other}
+    />
+  );
 }
 
 function renderSuggestion(suggestion, {query, isHighlighted}) {
-    const matches = match(suggestion.label, query);
-    const parts = parse(suggestion.label, matches);
+  console.log("suggestion", query,suggestion,isHighlighted);
+  const matches = match(suggestion.properties.name, query);
+  const parts = parse(suggestion.properties.name, matches);
+  console.log("matches", matches);
+  console.log("parts", parts);
 
-    return (
-        <MenuItem selected={isHighlighted} component="div">
-            <div>
-                {parts.map((part, index) => {
-                    return part.highlight ? (
-                        <span key={String(index)} style={{fontWeight: 500}}>
+  return (
+    <MenuItem selected={isHighlighted} component="div">
+      <div>
+        {parts.map((part, index) => {
+          return part.highlight ? (
+            <span key={String(index)} style={{fontWeight: 500}}>
               {part.text}
             </span>
-                    ) : (
-                        <strong key={String(index)} style={{fontWeight: 300}}>
-                            {part.text}
-                        </strong>
-                    );
-                })}
-            </div>
-        </MenuItem>
-    );
+          ) : (
+            <strong key={String(index)} style={{fontWeight: 300}}>
+              {part.text}
+            </strong>
+          );
+        })}
+
+        <strong key={1} style={{fontWeight: 300}}>
+          {" "}  - {suggestion.properties.country}
+        </strong>
+      </div>
+    </MenuItem>
+  );
 }
 
-function getApi(text) {
-    return "photon.komoot.de/api/?q=" + text + "&lang=fr&limit=8"
+
+function getSuggestions(data) {
+  let suggestions = data.features
+
+  console.log("suggestions", suggestions);
+  return suggestions
 }
 
-function getSuggestions(value) {
-    const inputValue = deburr(value.trim()).toLowerCase();
-    const inputLength = inputValue.length;
-    let count = 0;
-
-    return inputLength === 0
-        ? []
-        : suggestions.filter(suggestion => {
-            const keep =
-                count < 5 && suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
-
-            if (keep) {
-                count += 1;
-            }
-
-            return keep;
-        });
+function shouldRenderSuggestions(value) {
+  return value.trim().length > 2;
 }
 
 function getSuggestionValue(suggestion) {
-    return suggestion.label;
+  console.log(suggestion) //todo
+  return suggestion.properties.name;
 }
 
 
 class PlaceSearcher extends React.Component {
-    state = {
-        searchValue: '',
-        suggestions: [],
+
+  state = {
+    searchValue: '',
+    suggestions: [],
+  };
+
+
+  handleSuggestionsFetchRequested = ({value}) => {
+    console.log("fetch");
+    fetch("http://photon.komoot.de/api/?q=" + value + "&lang=fr&limit=4")
+      .then(rep => rep.json())
+      .then(data => this.setState({
+        suggestions: getSuggestions(data),
+      }))
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  handleSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: [],
+    });
+  };
+
+  handleChange = () => (event, {newValue}) => {
+    this.setState({
+      searchValue: newValue,
+    });
+  };
+
+  render() {
+    const {classes} = this.props;
+
+    const autosuggestProps = {
+      renderInputComponent,
+      suggestions: this.state.suggestions,
+      onSuggestionsFetchRequested: this.handleSuggestionsFetchRequested,
+      onSuggestionsClearRequested: this.handleSuggestionsClearRequested,
+      shouldRenderSuggestions,
+      getSuggestionValue,
+      renderSuggestion,
     };
 
-    handleSuggestionsFetchRequested = ({value}) => {
-        this.setState({
-            suggestions: getSuggestions(value),
-        });
-    };
-
-    handleSuggestionsClearRequested = () => {
-        this.setState({
-            suggestions: [],
-        });
-    };
-
-    handleChange = () => (event, {newValue}) => {
-        this.setState({
-            searchValue: newValue,
-        });
-    };
-
-    render() {
-        const {classes} = this.props;
-
-        const autosuggestProps = {
-            renderInputComponent,
-            suggestions: this.state.suggestions,
-            onSuggestionsFetchRequested: this.handleSuggestionsFetchRequested,
-            onSuggestionsClearRequested: this.handleSuggestionsClearRequested,
-            getSuggestionValue,
-            renderSuggestion,
-        };
-
-        return (
-            <div className={classes.root}>
-                <Autosuggest
-                    {...autosuggestProps}
-                    inputProps={{
-                        classes,
-                        label: "Search a place",
-                        value: this.state.searchValue,
-                        onChange: this.handleChange(),
-                    }}
-                    theme={{
-                        container: classes.container,
-                        suggestionsContainerOpen: classes.suggestionsContainerOpen,
-                        suggestionsList: classes.suggestionsList,
-                        suggestion: classes.suggestion,
-                    }}
-                    renderSuggestionsContainer={options => (
-                        <Paper {...options.containerProps} square>
-                            {options.children}
-                        </Paper>
-                    )}
-                />
-            </div>
-        );
-    }
+    return (
+      <div className={classes.root}>
+        <Autosuggest
+          {...autosuggestProps}
+          inputProps={{
+            classes,
+            label: "Search a place",
+            value: this.state.searchValue,
+            onChange: this.handleChange(),
+          }}
+          theme={{
+            container: classes.container,
+            suggestionsContainerOpen: classes.suggestionsContainerOpen,
+            suggestionsList: classes.suggestionsList,
+            suggestion: classes.suggestion,
+          }}
+          renderSuggestionsContainer={options => (
+            <Paper {...options.containerProps} square>
+              {options.children}
+            </Paper>
+          )}
+        />
+      </div>
+    );
+  }
 }
 
 PlaceSearcher.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(PlaceSearcher);
